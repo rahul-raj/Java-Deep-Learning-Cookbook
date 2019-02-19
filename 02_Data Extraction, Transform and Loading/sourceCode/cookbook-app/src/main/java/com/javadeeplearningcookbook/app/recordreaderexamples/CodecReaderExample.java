1c3aaf4e28ed55075951c4f1248d6614606d6de7
rraj <v-rraj@expedia.com>
2019-02-19 17:09:49 +0530

    add draft for codec reader example

---
+++ 02_Data Extraction, Transform and Loading/sourceCode/cookbook-app/src/main/java/com/javadeeplearningcookbook/app/recordreaderexamples/CodecReaderExample.java
@@ -0,0 +1,29 @@
+package com.javadeeplearningcookbook.app.recordreaderexamples;
+
+import org.datavec.api.conf.Configuration;
+import org.datavec.api.records.reader.SequenceRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.writable.Writable;
+import org.datavec.codec.reader.CodecRecordReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class CodecReaderExample {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        SequenceRecordReader codecRecordReader = new CodecRecordReader();
+        Configuration conf = new Configuration();
+        conf.set(CodecRecordReader.RAVEL, "true");
+        conf.set(CodecRecordReader.START_FRAME, "2");
+        conf.set(CodecRecordReader.TOTAL_FRAMES, "10");
+        conf.set(CodecRecordReader.ROWS, "80");
+        conf.set(CodecRecordReader.COLUMNS, "46");
+        //conf.set(CodecRecordReader.VIDEO_DURATION,"30");
+        codecRecordReader.initialize(new FileSplit(new File("D:/storage/Wildlife.mp4")));
+        codecRecordReader.setConf(conf);
+        List<List<Writable>> list =  codecRecordReader.sequenceRecord();
+        list.listIterator().forEachRemaining(el->System.out.println(el.size()));
+
+    }
+}
