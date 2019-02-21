a96bb63625250ccec266084b715a86034664f185
Rahul Raj <omrahulrajcse@gmail.com>
2019-02-21 08:07:35 +0530

    jackson line recorder example fix

--- 02_Data Extraction, Transform and Loading/sourceCode/cookbook-app/src/main/java/com/javadeeplearningcookbook/app/recordreaderexamples/JacksonLineRecordReaderExample.java
+++ 02_Data Extraction, Transform and Loading/sourceCode/cookbook-app/src/main/java/com/javadeeplearningcookbook/app/recordreaderexamples/JacksonLineRecordReaderExample.java
@@ -3,7 +3,10 @@ package com.javadeeplearningcookbook.app.recordreaderexamples;
 import org.datavec.api.conf.Configuration;
 import org.datavec.api.records.reader.impl.jackson.FieldSelection;
 import org.datavec.api.records.reader.impl.jackson.JacksonLineRecordReader;
+import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
 import org.datavec.api.split.FileSplit;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.schema.Schema;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.shade.jackson.core.JsonFactory;
@@ -11,9 +14,18 @@ import org.nd4j.shade.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class JacksonLineRecordReaderExample {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Schema schema = new Schema.Builder()
+                                .addColumnsDouble("sepalLength","sepalWidth","petalLength","petalWidth")
+                                .addColumnCategorical("species", Arrays.asList("setosa","versicolor","virginica"))
+                                .build();
+        TransformProcess transformProcess = new TransformProcess.Builder(schema)
+                                                .categoricalToInteger("species")
+                                                .build();
 
         FieldSelection fieldSelection = new FieldSelection.Builder()
                                   .addField("sepalLength")
@@ -22,12 +34,14 @@ public class JacksonLineRecordReaderExample {
                                   .addField("petalWidth")
                                   .addField("species")
                                   .build();
+
         JacksonLineRecordReader jacksonLineRecordReader = new JacksonLineRecordReader(fieldSelection,new ObjectMapper(new JsonFactory()));
         Configuration configuration = new Configuration();
         configuration.set(JacksonLineRecordReader.LABELS,"species");
         jacksonLineRecordReader.initialize(new FileSplit(new File("D:/storage/irisdata.txt")));
+        TransformProcessRecordReader recordReader = new TransformProcessRecordReader(jacksonLineRecordReader,transformProcess);
         System.out.println(jacksonLineRecordReader.next());
-        DataSetIterator dataSetIterator = new RecordReaderDataSetIterator(jacksonLineRecordReader,5,-1,3);
+        DataSetIterator dataSetIterator = new RecordReaderDataSetIterator(recordReader,5,-1,3);
         System.out.println(dataSetIterator.totalOutcomes());
 
 
