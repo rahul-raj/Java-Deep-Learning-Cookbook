2e7f3b16902faef461b85dea780c992203518827
Rahul Raj <omrahulrajcse@gmail.com>
2019-03-23 16:21:57 +0530

    add spring boot app and DL4J integration

---
+++ 03_Building Deep Neural Networks for Binary classification/sourceCode/spring-dl4j/src/main/java/com/springdl4j/springdl4j/service/CookBookService.java
@@ -0,0 +1,11 @@
+package com.springdl4j.springdl4j.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public interface CookBookService {
+    List<String> generateStringOutput(MultipartFile multipartFile) throws IOException, InterruptedException;
+}
