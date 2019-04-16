da18875c771518a6699836976a374d452e34d3fd
Rahul Raj <omrahulrajcse@gmail.com>
2019-04-16 22:55:52 +0530

    add google news vector example

---
+++ 05_Implementing NLP/sourceCode/cookbookapp/src/main/java/com/javadeeplearningcookbook/examples/GoogleNewsVectorExample.java
@@ -0,0 +1,16 @@
+package com.javadeeplearningcookbook.examples;
+
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class GoogleNewsVectorExample {
+
+    public static void main(String[] args) {
+        File file = new File("C:/Users/Admin/Downloads/GoogleNews-vectors-negative300.bin.gz");
+        Word2Vec model = WordVectorSerializer.readWord2VecModel(file);
+        System.out.println(Arrays.asList(model.wordsNearest("season",10)));
+    }
+}
